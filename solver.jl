3578e04f73809d3070de7caa8f815099cbf63476
HiroIshida <spitfire.docomo@gmail.com>
2019-03-17 18:02:22 +0900

    final

--- solver.jl
+++ solver.jl
@@ -13,66 +13,61 @@ eye(m) = eye(m, m)
     return p
 end
 
-struct MonteCarlo
-    func
-    x_min
-    x_max
-end
-
-function build_matrix(mc::MonteCarlo, pdf_target, Nx, Ny)
-    # func_mc : takes x as an argument and return random y following P(y|x) 
+function solve_integral_equation(func_mc, pdf_target, x_min, x_max, Nx, Ny)
 
     eps = 1e-3
     N_trial = 30000
 
     println("start mc")
-    x_grid_lst = make_grid_pts(Nx, mc.x_min, mc.x_max)
+    x_grid_lst = make_grid_pts(Nx, x_min, x_max)
     pt_result_lst = Vector{Float64}[]
     y_min = Inf; y_max = -Inf
     for x_grid in x_grid_lst
         for _ in 1:N_trial
-            y = mc.func(x_grid)
+            y = func_mc(x_grid)
             y < y_min && (y_min = y - eps)
             y > y_max && (y_max = y + eps)
             push!(pt_result_lst, [x_grid, y])
         end
     end
-    mesh = Grid2d([Nx, Ny], [mc.x_min, y_min], [mc.x_max, y_max])
+    mesh = Grid2d([Nx, Ny], [x_min, y_min], [x_max, y_max])
     data = collect_pts(mesh, pt_result_lst)
     println("finish mc")
 
     data_normalized = zeros(Nx, Ny)
-    dx = (mc.x_max - mc.x_min)/Nx
+    dx = (x_max - x_min)/Nx
     for i in 1:Nx
         data_normalized[i, :] = data[i, :]/sum(data[i, :])/dx
     end
-    #return data_normalized
 
     # then we can formulate integral-equation solving as 
     # least squares optimization with equality constraints
     A = data_normalized'*dx
     b = [pdf_target(y) for y in make_grid_pts(Ny, y_min, y_max)]
-    #w_opt = (inv(A'*A)*A'+eye(Nx)*0)*b
-    b = [pdf_target(y) for y in make_grid_pts(Ny, y_min, y_max)]
     C = ones(Nx)
     w = Variable(Nx)
-    λ = 0.01
+    λ = 0.001
     objective = sumsquares(A*w-b) + λ*sumsquares(w)
     constraints = [w>0; sum(w*dx) == 1]
     problem = minimize(objective, constraints)
     solve!(problem, SCSSolver())
     w_opt = w.value
+    #=
     plot(x_grid_lst, [normpdf(x, 0, 2*sqrt(2)) for x in x_grid_lst])
     plot(x_grid_lst, w_opt)
+    xlim(-15, 15)
+    =#
+    return x_grid_lst, w_opt
 end
 
 function test()
     npdf(x) = x + randn()
-    mc = MonteCarlo(npdf, -100, 100)
+    func_mc = npdf
+    x_min = -150
+    x_max = 150
     sigma = 3
     pdf_t(x) = 1/sqrt(2*pi*sigma^2)*exp(-(x-0)^2/(2*sigma^2))
-    Nx = 400
-    Ny = 400
-    mat = build_matrix(mc, pdf_t, Nx, Ny)
+    Nx = 1000
+    Ny = 1000
+    x_lst, w_lst = solve(func_mc, pdf_t, x_min, x_max, Nx, Ny)
 end
-mat = test()
